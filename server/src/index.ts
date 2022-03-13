612db36f44140401c1a953657f13395d679f3ff5
Suleyman <suleymanyaryev@gmail.com>
2022-03-13 22:05:44 +0500

    refactor(server): removed redundant steps

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,72 +29,15 @@ wss.on("connection", (ws, req) => {
     }
 
     const room = rooms.get(roomId)!;
-
-    ws.on("message", async (rawData) => {
-        const data = JSON.parse(rawData.toString());
-
-        if (data.type === "client-connected") {
-            console.log("new client connected");
-
-            for (const remoteClient of room) {
-                remoteClient.send(
-                    JSON.stringify({
-                        type: "client-connected",
-                    })
-                );
-            }
-            room.push(ws);
-        }
-
-        if (data.type === "client-offer") {
-            for (const remoteClient of room) {
-                if (remoteClient !== ws) {
-                    remoteClient.send(
-                        JSON.stringify({
-                            type: "client-offer",
-                            payload: {
-                                offer: data.payload.offer,
-                            },
-                        })
-                    );
-                }
-            }
-        }
-
-        if (data.type === "client-answer") {
-            for (const remoteClient of room) {
-                if (remoteClient !== ws) {
-                    remoteClient.send(
-                        JSON.stringify({
-                            type: "client-answer",
-                            payload: {
-                                answer: data.payload.answer,
-                            },
-                        })
-                    );
-                }
-            }
-        }
-
-        if (data.type === "client-ice-candidate") {
-            for (const remoteClient of room) {
-                if (remoteClient !== ws) {
-                    remoteClient.send(
-                        JSON.stringify({
-                            type: "client-ice-candidate",
-                            payload: {
-                                iceCandidate: data.payload.iceCandidate,
-                            },
-                        })
-                    );
-                }
+    ws.on("message", async (data) => {
+        for (const remoteClient of room) {
+            if (remoteClient !== ws) {
+                remoteClient.send(data);
             }
         }
     });
 
     ws.on("close", () => {
-        console.log("client disconnected");
-
         room.splice(
             room.findIndex((item) => item === ws),
             1
@@ -102,14 +45,6 @@ wss.on("connection", (ws, req) => {
 
         if (room.length === 0) {
             rooms.delete(roomId);
-        }
-
-        for (const remoteClient of room) {
-            remoteClient.send(
-                JSON.stringify({
-                    type: "client-disconnected",
-                })
-            );
         }
     });
 });
